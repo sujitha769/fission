ddd62178e5398c27ae813bafee3942378c8fab10
Sujitha Neelam <163429668+sujitha769@users.noreply.github.com>
2025-12-17 21:13:37 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
